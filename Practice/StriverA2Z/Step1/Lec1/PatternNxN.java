73caf2fc2cc92410bdd9990e88d19f48e6460a56
Harshi Jain <harshijain554@gmail.com>
2025-04-14 15:17:26 +0530

    Pattern-14/04/25

---
+++ Practice/StriverA2Z/Step1/Lec1/PatternNxN.java
@@ -0,0 +1,13 @@
+package Practice.StriverA2Z.Step1.Lec1;
+
+public class PatternNxN {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i =0 ; i < n ; i++){
+            for(int j =0 ; j < n ; j++){
+                System.out.print('*' + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Practice/StriverA2Z/Step1/Lec1/RightTriangleInNo.java
@@ -0,0 +1,7 @@
+package Practice.StriverA2Z.Step1.Lec1;
+
+public class RightTriangleInNo {
+    public static void main(String[] args) {
+        
+    }
+}
