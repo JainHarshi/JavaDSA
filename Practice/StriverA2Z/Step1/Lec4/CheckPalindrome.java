14e01730be099ed684cc0cedbc3928930f096ef6
Harshi Jain <harshijain554@gmail.com>
2025-04-20 22:40:03 +0530

    April20

--- Practice/StriverA2Z/Step1/Lec4/CheckPalindrome.java
+++ Practice/StriverA2Z/Step1/Lec4/CheckPalindrome.java
@@ -3,13 +3,16 @@ package Practice.StriverA2Z.Step1.Lec4;
 public class CheckPalindrome {
     public static void main(String[] args) {
         System.out.println();
-        int n = 123;
+        int n = 111;
+        int p =n;
         int rem = 0;
         while(n>0){
             int r = n%10;
             rem = rem*10 +r;
+            n = n/10;
         }
-        if(rem == n) System.out.println("True");
+        System.out.println(rem);
+        if(rem == p) System.out.println("True");
         else System.out.println("False");
     }
 }
