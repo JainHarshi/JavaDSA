49390a2a466e2546fea4f2301a73a964e320b098
Harshi Jain <harshijain554@gmail.com>
2025-04-24 01:54:44 +0530

    April23Coaching

---
+++ Coaching/April23/AbsoluteNo.java
@@ -0,0 +1,12 @@
+package Coaching.April23;
+import java.util.*;
+
+public class AbsoluteNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == Math.abs(n)) System.out.println("Absolute No");
+        else System.out.println(" Not Absolute No");
+
+    }
+}
