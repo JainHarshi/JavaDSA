d2ac9a8b109ea3bcb8c936899920ab6c4b103f2a
Harshi Jain <harshijain554@gmail.com>
2025-04-24 03:00:38 +0530

    RecurrApril24

--- Practice/StriverA2Z/Step1/Lec5/Factorial.java
+++ Practice/StriverA2Z/Step1/Lec5/Factorial.java
@@ -1,1 +1,15 @@
+package Practice.StriverA2Z.Step1.Lec5;
 
+public class Factorial {
+    public static void main(String[] args) {
+       System.out.println(factorial(5)); 
+    }
+    public static int factorial(int N) {
+        if(N == 0 || N== 1) return 1;
+        return N * factorial(N-1);
+        
+
+    }
+
+
+}
