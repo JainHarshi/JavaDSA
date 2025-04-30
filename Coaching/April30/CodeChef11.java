876361b7170911f0b053a8cad244950eadef80f4
Harshi Jain <harshijain554@gmail.com>
2025-04-30 23:26:43 +0530

    May1CoachingCodeChef

---
+++ Coaching/April30/CodeChef11.java
@@ -0,0 +1,14 @@
+package Coaching.April30;
+
+import java.util.Scanner;
+
+public class CodeChef11 {
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int n1 = sc.nextInt();
+        System.out.println(n-n1);
+
+		}
+}
