67056e15758c22b2a6528e72fd51410c9d463484
Harshi Jain <harshijain554@gmail.com>
2025-05-03 04:01:08 +0530

    May2Coaching

---
+++ Coaching/April30/CodeChef33.java
@@ -0,0 +1,20 @@
+package Coaching.April30;
+
+import java.util.Scanner;
+
+public class CodeChef33 {
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    while(n-- >0){
+	        int n1 = sc.nextInt();
+	        int n2 = sc.nextInt();
+	        if(n1<n2)System.out.println("Yes");
+	        else System.out.println("No");
+	        
+	    }
+	}
+
+    // Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+}
---
+++ Coaching/April30/CodeChef36.java
@@ -0,0 +1,21 @@
+package Coaching.April30;
+
+import java.util.Scanner;
+
+public class CodeChef36 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    while(n-- >0){
+	        int n1 = sc.nextInt();
+	        int n2 = sc.nextInt();
+	        if(n1>n2)System.out.println("Loss");
+	        else if(n1==n2)System.out.println("Neutral");
+	        else if(n1<n2)System.out.println("Profit");
+	        
+	    }
+	}
+}
+
+// Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BULLBEAR
