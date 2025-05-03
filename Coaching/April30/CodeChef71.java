7e75a04bd05b3aebf6aff92a7ec29169cba4abb6
Harshi Jain <harshijain554@gmail.com>
2025-05-04 02:20:10 +0530

    May3CodeChefCoaching

---
+++ Coaching/April30/CodeChef71.java
@@ -0,0 +1,25 @@
+ package Coaching.April30;
+
+import java.util.Scanner;
+
+public class CodeChef71 {
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc = new Scanner(System.in);
+	   // int n = sc.nextInt();
+	   // while(n-- >0){
+	        int n1 = sc.nextInt();
+	        int n2 = sc.nextInt();
+	        int n3 = sc.nextInt();
+	        if(n1+n3 >= n2)System.out.println("YES");
+	        else System.out.println("NO");
+	       // else if(n3>=n1 && n2>= n3)System.out.println(n3);
+	        
+	    }
+    
+}
+
+
+
+
+// Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
---
+++ Coaching/April30/CodeChef72.java
@@ -0,0 +1,20 @@
+ package Coaching.April30;
+
+import java.util.Scanner;
+
+public class CodeChef72 {
+public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    while(n-- >0){
+	        int n1 = sc.nextInt();
+	        int n2 = sc.nextInt();
+	       // int n3 = sc.nextInt();
+	        System.out.println(n1-n2);
+	       // else  if(n1 >3 && n1 <= 6) System.out.println("SILVER");
+	       //else System.out.println("GOLD");
+	}
+	}
+//   Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TFPAPER  
+}
