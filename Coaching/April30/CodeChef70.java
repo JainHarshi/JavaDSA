9ed7a9aacdf3bb051f1cd729781a532c93864d71
Harshi Jain <harshijain554@gmail.com>
2025-05-03 23:27:18 +0530

    May3CoachingCodeChef

---
+++ Coaching/April30/CodeChef70.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CodeChef70 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    while(n-- >0){
+	        int n1 = sc.nextInt();
+	        int n2 = sc.nextInt();
+	       // int n3 = sc.nextInt();
+	       // int n4 = sc.nextInt();
+	        if(n1 >= n2*2)System.out.println("Yes");
+	        else System.out.println("No");
+	       // else System.out.println("Ronaldo");
+	        	
+	}
+	}
+}
+
+// Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INVESTMENT
