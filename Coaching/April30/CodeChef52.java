f88be8aeaa8d59880733e529187d6812c9fa08de
Harshi Jain <harshijain554@gmail.com>
2025-05-03 18:27:00 +0530

    May3CodechefCoaching

---
+++ Coaching/April30/CodeChef52.java
@@ -0,0 +1,15 @@
+public class CodeChef52 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    while(n-- >0){
+	        int n1 = sc.nextInt();
+	       // int n2 = sc.nextInt();
+	        System.out.println((n1*2));
+	       // else System.out.println("No");
+	        
+	    }
+}
+
+// Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
---
+++ Coaching/April30/CodeChef53.java
@@ -0,0 +1,15 @@
+public class CodeChef53 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    while(n-- >0){
+	        int n1 = sc.nextInt();
+	       // int n2 = sc.nextInt();
+	        if(n1>= 1 && n1 <= 4)System.out.println(("YES"));
+	        else System.out.println("No");
+	        
+	    }
+}
+
+// Q- https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
