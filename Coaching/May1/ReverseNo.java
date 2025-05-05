ff2c30c61db05d7188f95a92bcc6a58dce042005
Harshi Jain <harshijain554@gmail.com>
2025-05-06 00:23:48 +0530

    May5Coaching

--- Coaching/May1/ReverseNo.java
+++ Coaching/May1/ReverseNo.java
@@ -1,6 +1,9 @@
+package Coaching.May1;
+
 public class ReverseNo {
     
     public static void main(String[] args) {
+        int n = 1234;
         while(n>0){
             System.out.println(n%10);
             n= n/10;
--- Coaching/May1/SumOfDigit.java
+++ Coaching/May1/SumOfDigit.java
@@ -2,7 +2,8 @@ package Coaching.May1;
 
 public class SumOfDigit {
     public static void main(String[] args) {
-        sum =0;
+        int sum =0;
+        int n = 12345;
         while(n>0){
             System.out.println(sum +=n%10);
             n= n/10;
