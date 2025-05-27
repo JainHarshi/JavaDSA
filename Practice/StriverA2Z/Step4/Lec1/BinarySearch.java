bdcfdf44630c3d0d86b7e9ff2203c51647e4e7d9
Harshi Jain <harshijain554@gmail.com>
2025-05-28 02:51:59 +0530

    May27Practice

--- Practice/StriverA2Z/Step4/Lec1/BinarySearch.java
+++ Practice/StriverA2Z/Step4/Lec1/BinarySearch.java
@@ -1,7 +1,25 @@
-package Practice.StriverA2Z.Step4.Lec1;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        System.out.println(search(new int[]{1,2,3,4}, 4)); 
+ 
+    }
+    public static int search(int[] nums, int target) {
+        int s =0;
+        int e = nums.length-1;
+        while(e>= s){
+            int mid = s + ((e-s)/2);
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                e= mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return -1;
         
     }
 }
--- Practice/StriverA2Z/Step4/Lec1/BruteBinarySearch.java
+++ Practice/StriverA2Z/Step4/Lec1/BruteBinarySearch.java
@@ -1,5 +1,13 @@
 public class BruteBinarySearch {
     public static void main(String[] args) {
-        
+       System.out.println(search(new int[]{1,2,3,4}, 4)); 
+    }
+        public static int search(int[] nums, int target) {
+        for(int i =0 ; i<nums.length ; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
     }
 }
