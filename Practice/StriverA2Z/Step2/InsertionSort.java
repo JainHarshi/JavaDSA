ed3aa8413b1cdb58c6bcfaaa148b92c8068fad48
Harshi Jain <harshijain554@gmail.com>
2025-06-07 19:36:37 +0530

    June7

--- Practice/StriverA2Z/Step2/InsertionSort.java
+++ Practice/StriverA2Z/Step2/InsertionSort.java
@@ -5,19 +5,31 @@ import java.util.Arrays;
 public class InsertionSort {
     
     public static void main(String args[]){
-        int[] arr = new int[]{1,2,8,3,1};
+        // int[] arr = new int[]{1,1,3,4,5};
+        // int[] arr = new int[]{10, 1, 2, 3, 4};
+        // int[] arr = new int[]{10, 9, 8, 7, 6, 5};
+        // int[] arr = new int[]{4, 2, 1, 3};
+        // int[] arr = new int[]{3, 5, 4, 1, 2};
+        int[] arr = new int[]{2, 1, 4, 3, 6, 5, 8, 7, 10, 9};
+
+
+
+
+
+        // int[] arr = new int[]{5, 4, 3, 2, 1};
+
         int n = arr.length;
-        for(int i =0 ; i<n-1 ; i++){
-            while(arr[i+1]< arr[i] && i<n-1){
-                swap(arr[i+1], arr[i]);
+        for(int i =1 ; i<n ; i++){
+            while(i<n  && i>0 && arr[i-1] > arr[i] ){
+                swap(arr,i-1, i);
                 i =i-1;
             }
         }
         System.out.println(Arrays.toString(arr));
     }
-    public static void swap(int n , int m){
-        int temp = n;
-        n =m;
-        m = temp;
+    public static void swap(int[] arr, int n , int m){
+        int temp = arr[n];
+        arr[n] =arr[m];
+        arr[m] = temp;
     }
 }
