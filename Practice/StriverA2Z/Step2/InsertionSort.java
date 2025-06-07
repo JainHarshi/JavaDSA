024d484cbb09eeb73bb024f2dab3842130ccb9ff
Harshi Jain <harshijain554@gmail.com>
2025-06-07 18:44:53 +0530

    June7

--- Practice/StriverA2Z/Step2/InsertionSort.java
+++ Practice/StriverA2Z/Step2/InsertionSort.java
@@ -1,5 +1,23 @@
 package Practice.StriverA2Z.Step2;
 
+import java.util.Arrays;
+
 public class InsertionSort {
     
+    public static void main(String args[]){
+        int[] arr = new int[]{1,2,8,3,1};
+        int n = arr.length;
+        for(int i =0 ; i<n-1 ; i++){
+            while(arr[i+1]< arr[i] && i<n-1){
+                swap(arr[i+1], arr[i]);
+                i =i-1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void swap(int n , int m){
+        int temp = n;
+        n =m;
+        m = temp;
+    }
 }
