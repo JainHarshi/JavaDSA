497870c2ffbb4c5bc92c60daafa3c08dab567da7
Harshi Jain <harshijain554@gmail.com>
2025-06-08 14:41:38 +0530

    June8

---
+++ Practice/KunalRecursion/PrintSeedhaTriangleRecursion.java
@@ -0,0 +1,21 @@
+package Practice.KunalRecursion;
+
+public class PrintSeedhaTriangleRecursion {
+    public static void main(String[] args) {
+        
+    }
+    public static void triangle(int r, int c){
+        if(r==0) return;
+        if(r>c){
+            triangle(r, c+1);
+            System.out.print("*");
+
+        }
+        else{
+            triangle(r-1, 0);
+            System.out.println();
+
+        }
+
+    }
+}
