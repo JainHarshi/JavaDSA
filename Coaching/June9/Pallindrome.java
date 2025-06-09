d6b6f50c9630a4ce1040ed34533277a8b7a03f8b
Harshi Jain <harshijain554@gmail.com>
2025-06-10 01:45:53 +0530

    June9

--- Coaching/June9/Pallindrome.java
+++ Coaching/June9/Pallindrome.java
@@ -1,5 +1,14 @@
 package Coaching.June9;
 
 public class Pallindrome {
-    
+   public static void main(String[] args) {
+    int n = 131;
+    int ans =0;
+    int temp = n;
+    while(n>0){
+        ans= ans*10 + n%10;
+        n = n/10; 
+    }
+    System.out.println(ans == temp ? "Pallindrome" : "Non Pallindrome");
+   } 
 }
