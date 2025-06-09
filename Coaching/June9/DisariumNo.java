caf24ba8727c3c90f7bc2b6c871c62bb078a84c4
Harshi Jain <harshijain554@gmail.com>
2025-06-10 02:26:14 +0530

    June9

--- Coaching/June9/DisariumNo.java
+++ Coaching/June9/DisariumNo.java
@@ -2,7 +2,7 @@ package Coaching.June9;
 
 public class DisariumNo {
     public static void main(String[] args) {
-        int n =89;
+        int n =75;
         int count =0;
         int temp = n;
         int ans =0;
@@ -14,6 +14,7 @@ public class DisariumNo {
         }
         while(temp >0){
             ans += (int)Math.pow(temp%10, count--);
+            temp /= 10;
         }
         System.out.println(temp2 == ans ? "Disarium No ": "Non Disarium No");
 
