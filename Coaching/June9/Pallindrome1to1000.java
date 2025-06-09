607c08d27ace3541191523cd4684c111718518c0
Harshi Jain <harshijain554@gmail.com>
2025-06-10 02:28:08 +0530

    June9

--- Coaching/June9/Pallindrome1to1000.java
+++ Coaching/June9/Pallindrome1to1000.java
@@ -2,7 +2,8 @@ package Coaching.June9;
 
 public class Pallindrome1to1000 {
     public static void main(String[] args) {
-        for (int n = 1; n <= 1000; n++) {
+        for (int i = 1; i <= 1000; i++) {
+            int n =i;
             
             int ans = 0;
             int temp = n;
@@ -10,7 +11,7 @@ public class Pallindrome1to1000 {
                 ans = ans * 10 + n % 10;
                 n = n / 10;
             }
-            System.out.println(ans == temp ? n + " Pallindrome" : n + " Non Pallindrome");
+            System.out.println(ans == temp ? i + " Pallindrome" : i + " Non Pallindrome");
             n = temp;
         }
     }
