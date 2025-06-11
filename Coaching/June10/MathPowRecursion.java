a89a5f6818c13f6303bc13b70d8c493135c78d36
Harshi Jain <harshijain554@gmail.com>
2025-06-11 22:36:10 +0530

    June11

--- Coaching/June10/MathPowRecursion.java
+++ Coaching/June10/MathPowRecursion.java
@@ -2,7 +2,26 @@ package Coaching.June10;
 
 public class MathPowRecursion {
     public static void main(String[] args) {
-        
+        System.out.println(myPow(2, 0));
+    }
+    public static double myPow(double x, int n) {
+        long N = n;
+        if(N<0){
+            N = -N;
+            x = 1/x;
+        }
+       return powhalf(x,N);
+    }
+    public static double powhalf(double x , long N){
+        if(N == 0) return 1.0;
+        double half = powhalf(x, N/2);
+
+        if(N%2 == 0) {
+            return half *half;
+            }
+        else{
+            return half*half*x;
+        }
     }
 
 }
