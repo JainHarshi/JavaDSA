5f33ff22dc550c1491af0a30254058e3e235dcaa
Harshi Jain <harshijain554@gmail.com>
2025-06-19 23:09:40 +0530

    June19

---
+++ Practice/StriverA2Z/Step3/Lec1/UnionOfSortedArray.java
@@ -0,0 +1,26 @@
+package Practice.StriverA2Z.Step3.Lec1;
+
+import java.util.Arrays;
+
+public class UnionOfSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int[] nums2 = {1, 2, 7};
+        int i =0;
+        int j =0;
+        int m =0;
+        int[] ans = new int[nums1.length + nums2.length];
+        while(i<nums1.length && j<nums2.length ){
+            if(nums1[i] <= nums2[j]){
+                ans[m++] = nums1[i];
+                i++;
+            }
+            else{
+                ans[m++] = nums2[j];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+
+    }
+}
