8195dc9004fa54e4305dd9a39767c8a87ce9e448
Harshi Jain <harshijain554@gmail.com>
2025-06-20 01:02:12 +0530

    June19

--- Practice/StriverA2Z/Step3/Lec2/SortColor.java
+++ Practice/StriverA2Z/Step3/Lec2/SortColor.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class SortColor {
     public static void main(String[] args) {
-        
+        int[] nums = {2,2,1};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums));
     }
         public static void sortColors(int[] nums) {
         Arrays.sort(nums);
