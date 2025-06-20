0a85199e6e4e46aedacac6b11f4bf64519b2814b
Harshi Jain <harshijain554@gmail.com>
2025-06-21 00:58:46 +0530

    June20

--- Practice/StriverA2Z/Step3/Lec1/SortArrayBetter.java
+++ Practice/StriverA2Z/Step3/Lec1/SortArrayBetter.java
@@ -1,9 +1,12 @@
 package Practice.StriverA2Z.Step3.Lec1;
 
+import java.util.Arrays;
+
 public class SortArrayBetter {
     public static void main(String[] args) {
         int nums[] = {2,0,2,1,1,0};
         sortColors(nums); 
+        System.out.println(Arrays.toString(nums));
     }
     public static void sortColors(int[] nums) {
         int cnt0 =0;
