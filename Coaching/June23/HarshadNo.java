7a8b3785727f12f484b5dfddf44a87a864639f01
Harshi Jain <harshijain554@gmail.com>
2025-06-24 00:57:06 +0530

    June23

--- Coaching/June23/HarshadNo.java
+++ Coaching/June23/HarshadNo.java
@@ -1,12 +1,23 @@
 package Coaching.June23;
 
+import Coaching.May1.SumOfDigit;
+
 public class HarshadNo {
     public static void main(String[] args) {
-        System.out.println(HarshadNo(0, 0));
+        int n = 18;
+        int div = 0;
+        System.out.println(HarshadNo(n, div));
     }
     public static boolean HarshadNo(int n ,int div){
         
-        return HarshadNo(0, 0);
+        int sum = sumOfDigit(n , 0);
 
+         return n%div == 0;
+
+    }
+    public static int sumOfDigit(int n, int sum){
+        if(n == 0) return sum;
+        sum += n%10;
+        return sumOfDigit(n/10 , sum);
     }
 }
