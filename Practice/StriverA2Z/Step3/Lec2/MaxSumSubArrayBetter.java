fcaaf6c73003c16f0039bfc11ca665de75393ec8
Harshi Jain <harshijain554@gmail.com>
2025-06-24 01:50:05 +0530

    June23

--- Practice/StriverA2Z/Step3/Lec2/MaxSumSubArrayBetter.java
+++ Practice/StriverA2Z/Step3/Lec2/MaxSumSubArrayBetter.java
@@ -1,5 +1,28 @@
 package Practice.StriverA2Z.Step3.Lec2;
 
 public class MaxSumSubArrayBetter {
-    
+    public static void main(String[] args) {
+        int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+        System.out.println(maxSubArray(nums));
+    }
+    // class Solution {
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i =0 ; i<nums.length ; i++){
+            int sum =0;
+            for(int j =i ; j<nums.length ; j++){
+                
+                // for(int k = i ; k <= j ; k++){
+                    sum += nums[j];
+                    if(max < sum){
+                        max = sum;
+                    // }
+
+                }
+            }
+        }
+        return max;
+    }
 }
+// }
