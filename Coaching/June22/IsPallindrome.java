3be54cc6d011f0d10ee9ffa9967ce6900b1f7d98
Harshi Jain <harshijain554@gmail.com>
2025-06-23 23:54:54 +0530

    June23

--- Coaching/June22/IsPallindrome.java
+++ Coaching/June22/IsPallindrome.java
@@ -1,3 +1,22 @@
 public class IsPallindrome {
-    
+    public static void main(String[] args) {
+        int n = 1213;
+      int rev = Pallindrome(n , 0);  
+      if(rev == n){
+        System.out.println(n + "  is the  Pallindone");
+      }
+      else{
+        System.out.println(n + "  is not a the  Pallindone");
+
+      }
+    }
+    public static int Pallindrome(int n , int rev){
+        if(n == 0){
+            return rev;
+             
+        }
+        rev = rev*10 + n%10;
+        return Pallindrome(n/10, rev);
+
+    }
 }
