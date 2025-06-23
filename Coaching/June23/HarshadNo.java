458f927fe88d0c238813e26fde82d4899c47c25a
Harshi Jain <harshijain554@gmail.com>
2025-06-24 00:49:32 +0530

    June23

---
+++ Coaching/June23/HarshadNo.java
@@ -0,0 +1,12 @@
+package Coaching.June23;
+
+public class HarshadNo {
+    public static void main(String[] args) {
+        System.out.println(HarshadNo(0, 0));
+    }
+    public static boolean HarshadNo(int n ,int div){
+        
+        return HarshadNo(0, 0);
+
+    }
+}
