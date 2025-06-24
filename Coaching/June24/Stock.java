4f10421619a6ccf09855b381937c5b3ba2b0f907
Harshi Jain <harshijain554@gmail.com>
2025-06-25 02:18:32 +0530

    June24

--- Coaching/June24/Stock.java
+++ Coaching/June24/Stock.java
@@ -1,5 +1,7 @@
 package Coaching.June24;
 
 public class Stock {
-    
+    public static void main(String[] args) {
+        System.out.println("Stock is a kinda DP question just so we storing minimum ele from the given array");
+    }
 }
