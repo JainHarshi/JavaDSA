fbede23df3e9ebd1b7e700025331136cb7a436f5
Harshi Jain <harshijain554@gmail.com>
2025-06-26 03:32:59 +0530

    June25

---
+++ Practice/StriverA2Z/Step3/Lec2/RearrangeArray.java
@@ -0,0 +1,36 @@
+package Practice.StriverA2Z.Step3.Lec2;
+
+import java.util.*;
+
+public class RearrangeArray {
+    public static int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int pos[] = new int[n/2];
+        int neg[] = new int[n/2];
+        int p = 0;
+        int n1 = 0;
+        for(int i =0 ; i<n ; i++){
+            if(nums[i] > 0){
+                pos[p++] = nums[i];
+            }
+            else{
+               neg[n1++] = nums[i]; 
+            }
+
+        }
+        p=0;
+        n1=0;
+        for(int i = 0 ; i<n ; i++){
+            if(i%2 == 0){
+            nums[i] = pos[p++];}
+            else{
+                nums[i] = neg[n1++];
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int[] prices = {-17,-1,-5,3,6,4};
+        System.out.println(Arrays.toString(rearrangeArray(prices)));
+    }
+}
