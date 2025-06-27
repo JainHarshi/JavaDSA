f4723ee59909e1dd8ebecb150ac4d2bc71e32581
Harshi Jain <harshijain554@gmail.com>
2025-06-28 01:14:02 +0530

    June27

---
+++ Coaching/June27/BetterMaxEle.java
@@ -0,0 +1,19 @@
+package Coaching.June27;
+
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class BetterMaxEle {
+    public static void max(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int ele : arr){
+            pq.add(ele);
+        }
+        System.out.println(pq.peek());
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,2,4,3,2,13,1,3,1,3,1};
+        max(arr);
+    }
+}
