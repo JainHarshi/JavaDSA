81ab28f0d218f637e1b41719d3271f2d928dfdf8
Harshi Jain <harshijain554@gmail.com>
2025-07-01 07:59:51 +0530

    July1

--- Coaching/July1/PreorderRecursive.java
+++ Coaching/July1/PreorderRecursive.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// import javax.swing.tree.TreeNode;
+
 public class PreorderRecursive {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(preorderTraversal(root));
+    }
     public static List<Integer> preorderTraversal(TreeNode root) {
         ArrayList<Integer> ans = new ArrayList<Integer>();
         preorder( root, ans);
@@ -14,3 +25,13 @@ public class PreorderRecursive {
 
     }
 }
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
