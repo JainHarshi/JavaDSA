9841f18b5cbc281f7ac8cf8ad69d9c7a0a53ad3a
Harshi Jain <harshijain554@gmail.com>
2025-07-01 16:18:16 +0530

    July1

--- Coaching/July1/LevelOrderTraversal.java
+++ Coaching/July1/LevelOrderTraversal.java
@@ -1,5 +1,57 @@
 package Coaching.July1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LevelOrderTraversal {
-    
+
+    public static void main(String[] args) {
+        // Create sample tree: [1, 2, 3, 4, 5]
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        List<List<Integer>> result = levelOrder(root);
+        System.out.println("Level Order Traversal: " + result);
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if (root == null) return ans;
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            ArrayList<Integer> lvl = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                lvl.add(node.val);
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+
+            ans.add(lvl);
+        }
+
+        return ans;
+    }
+}
+
+// TreeNode class
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
 }
