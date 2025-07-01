d0e0dfb1ce18947005f241ffdfd04e14e242602b
Harshi Jain <harshijain554@gmail.com>
2025-07-01 16:26:51 +0530

    July1

--- Coaching/July1/BalanceBinaryTree.java
+++ Coaching/July1/BalanceBinaryTree.java
@@ -1,5 +1,33 @@
-package Coaching.July1;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left-right) > 1) return false;
+        // return true; yha par main only root node k liye check kr rh thi but sabhi k liye check krna h
+        return isBalanced(root.left) && isBalanced(root.right);
 
-public class BalanceBinaryTree {
-    
+        
+    }
+    public static int height(TreeNode root){
+        if(root == null) return 0;
+        int left =  height(root.left);
+        int right = height(root.right);
+        return 1+ Math.max(left,right);
+    }
 }
