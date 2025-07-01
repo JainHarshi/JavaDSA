5de6ae4202156beee1d9333cd501b9d3d66fa8f5
Harshi Jain <harshijain554@gmail.com>
2025-07-01 16:30:32 +0530

    July1

--- Coaching/July1/DiameterTree.java
+++ Coaching/July1/DiameterTree.java
@@ -1,5 +1,21 @@
 package Coaching.July1;
 
 public class DiameterTree {
-    
+    public static void main(String[] args) {
+        // Sample tree:
+        //         1
+        //        / \
+        //       2   3
+        //      / \     
+        //     4   5  
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution sol = new Solution();
+        int diameter = sol.diameterOfBinaryTree(root);
+        System.out.println("Diameter of the Binary Tree: " + diameter);  // Output should be 3
+    }
 }
