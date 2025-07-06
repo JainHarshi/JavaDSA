000622d251ec12c1fd913b2f55cfe06f47dcfd32
Harshi Jain <harshijain554@gmail.com>
2025-07-06 14:43:33 +0530

    July6

--- Coaching/July5/preorderBy2Stacks.java
+++ Coaching/July5/preorderBy2Stacks.java
@@ -2,4 +2,6 @@ public class preorderBy2Stacks {
     public static void main(String[] args) {
         
     }
+
+    // One stack to push data and then put that into st2 and put its left and right child simmilarly
 }
